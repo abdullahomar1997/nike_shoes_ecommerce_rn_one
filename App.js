3aa5be2c67e2a56b44a83f8fd8e43c7473947416
Avatar Tas <abdullahomar19971201@gmail.com>
2023-08-06 22:17:12 +0200

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,118 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import React, { useEffect, useState } from 'react';
+import * as Font from 'expo-font';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+import {
+    Image,
+    TouchableOpacity
+} from 'react-native';
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
+
+// screens
+import { Home } from "./screens/";
+
+import { images, icons, COLORS, FONTS, SIZES } from './constants';
+
+const theme = {
+    ...DefaultTheme,
+    colors: {
+        ...DefaultTheme.colors,
+        border: "transparent",
+    },
+};
+
+const Stack = createStackNavigator();
+
+const App = () => {
+
+    const [fontLoaded, setFontLoaded] = useState(false);
+
+    useEffect(() => {
+        // Load the custom font asynchronously
+        const loadFont = async () => {
+          await Font.loadAsync({
+            'CarmenSans-Regular': require('./assets/fonts/CarmenSans-Regular.otf'),
+            // 'CarmenSans-Black': require('./assets/fonts/'),
+            // 'CarmenSans-Bold': require('./assets/fonts/'),
+            'CarmenSans-Thin': require('./assets/fonts/CarmenSans-Thin.otf'),
+            'CarmenSans-SemiBold': require('./assets/fonts/CarmenSans-SemiBold.otf'),
+            'CocoGothic': require('./assets/fonts/CocoGothic.ttf'),
+            // Add more custom fonts here if needed
+          });
+          setFontLoaded(true);
+        };
+    
+        loadFont();
+      }, []);
+
+      if (!fontLoaded) {
+        // Return a loading screen or null while the font is loading
+        return null;
+      }
+
+    return (
+        <NavigationContainer theme={theme}>
+            <Stack.Navigator
+                initialRouteName={'Home'}
+            >
+                <Stack.Screen
+                    name="Home"
+                    component={Home}
+                    options={{
+                        title: 'SHOE SELECTOR',
+                        headerStyle: {
+                            //backgroundColor: '#f4511e',
+                        },
+                        headerTintColor: COLORS.lightGray,
+                        headerTitleStyle: {
+                            ...FONTS.navTitle
+                        },
+                        headerLeft: ({ onPress }) => (
+                            <TouchableOpacity
+                                style={{ marginLeft: SIZES.padding }}
+                                onPress={onPress}
+                            >
+                                <Image
+                                    source={icons.menu}
+                                    resizeMode="contain"
+                                    style={{
+                                        width: 25,
+                                        height: 25,
+                                    }}
+                                />
+                            </TouchableOpacity>
+                        ),
+                        headerRight: () => (
+                            <TouchableOpacity
+                                style={{ marginRight: SIZES.padding }}
+                                onPress={() => console.log("Pressed")}
+                            >
+                                <Image
+                                    source={icons.search}
+                                    resizeMode="contain"
+                                    style={{
+                                        width: 30,
+                                        height: 30,
+                                    }}
+                                />
+                            </TouchableOpacity>
+                        ),
+                    }}
+                />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+};
+
+
+export default () => {
+    return <App />;
+};
